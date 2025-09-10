5a99d65e8f577d903a4f266fbce920128845dfdb
Saladin Shaurov <salah@SS-Macbook-Air.local>
2025-09-09 18:15:46 -0600

    Implemented reduce method

--- src/lab1/Reduce.java
+++ src/lab1/Reduce.java
@@ -1,4 +1,22 @@
 package lab1;
 
 public class Reduce {
+    public static void main(String[] args) {
+        System.out.println(reduce(1000));
+    }
+
+    public static int reduce(int n) {
+        int count = 0;
+        while (n != 0) {
+
+            if (n % 2 == 0) {
+                n /= 2;
+            }
+            else {
+                n--;
+            }
+            count++;
+        }
+        return count;
+    }
 }
